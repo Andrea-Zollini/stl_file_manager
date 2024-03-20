408510f18e30d4df0f2388b89a4ab429a863a101
andrea <andrea.zollini@proton.me>
2024-03-20 08:27:55 +0100

    db seeder for local development

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -23,5 +23,12 @@ class DatabaseSeeder extends Seeder
             'password' => Hash::make('12345678'),
             'remember_token' => Str::random(10),
         ]);
+        User::create([
+            'name' => 'user',
+            'email' => 'user@test.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make('12345678'),
+            'remember_token' => Str::random(10),
+        ]);
     }
 }
