bc37c3de06c4d0523c2c5218c0b56aae18a50679
andrea <andrea.zollini@proton.me>
2024-03-22 09:03:33 +0100

    seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -23,12 +23,32 @@ class DatabaseSeeder extends Seeder
             'password' => Hash::make('12345678'),
             'remember_token' => Str::random(10),
         ]);
-        // User::create([
-        //     'name' => 'user',
-        //     'email' => 'user@test.com',
-        //     'email_verified_at' => now(),
-        //     'password' => Hash::make('12345678'),
-        //     'remember_token' => Str::random(10),
-        // ]);
+        User::create([
+            'name' => 'utente1',
+            'last_name' => 'cognome',
+            'phone_number' => '3210987654',
+            'email' => 'utente1@test.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make('12345678'),
+            'remember_token' => Str::random(10),
+        ]);
+        User::create([
+            'name' => 'utente2',
+            'last_name' => 'cognome2',
+            'phone_number' => '3210987655',
+            'email' => 'utente2@test.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make('12345678'),
+            'remember_token' => Str::random(10),
+        ]);
+        User::create([
+            'name' => 'utente3',
+            'last_name' => 'cognome3',
+            'phone_number' => '3210987656',
+            'email' => 'utente3@test.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make('12345678'),
+            'remember_token' => Str::random(10),
+        ]);
     }
 }
